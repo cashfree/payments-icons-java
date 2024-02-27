e1e1bf3d58456a525d2e0ad8d0f78982dd8a271a
Kishan_maurya <kishan.maurya@cashfree.com>
2024-02-27 19:05:10 +0530

    Release pipeline setup

--- PaymentIcons/build.gradle.kts
+++ PaymentIcons/build.gradle.kts
@@ -1,7 +1,15 @@
 plugins {
-    id("java-library")
+    id("java")
     id("org.jetbrains.kotlin.jvm")
 }
+
+extra.apply {
+    set("PUBLISH_GROUP_ID", "com.cashfree.pg")
+    set("PUBLISH_ARTIFACT_ID", "icons")
+    set("PUBLISH_VERSION", "1.0.0")
+}
+
+apply(from = "${rootProject.projectDir}/scripts/publish-module.gradle")
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,15 @@ plugins {
     id("com.android.application") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("org.jetbrains.kotlin.jvm") version "1.9.0" apply false
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0" apply true
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            stagingProfileId.set(System.getenv("SONATYPE_STAGING_PROFILE_ID"))
+            username.set(System.getenv("MAVEN_USERNAME"))
+            password.set(System.getenv("MAVEN_PASSWORD"))
+        }
+    }
 }
